a28517096163cc30eab87791efb1478fc53240b8
ngomezKS <naimgomezcn@gmail.com>
2022-02-25 20:13:51 +0100

    log system

--- crypto/crypto/crypto/utils/logger.py
+++ crypto/crypto/crypto/utils/logger.py
@@ -6,20 +6,25 @@ def sprint(*argv):
     print("\n")
     print("="*20)
     print("\n")
+    
+    rprint = ""
     for i in argv:
-        print(i, end=" ")
+        rprint += i
         
+    print(rprint, end=" ")
+    log(rprint, sprint_log=True)
+    
     print("\n")
     print("="*20)
     print("\n")
     print("\n")
 
 
-def log(*argv):
+def log(*argv, sprint_log = False):
     
     folder = "logs"
-    master_log = folder + '/' + 'master_log.txt'
-    today_log = folder + '/' + 'master_log.txt'
+    master_log = folder + '/' + 'master.log'
+    today_log = folder + '/' + str(datetime.today().strftime('%Y-%m-%d')) + '.log'
     
     if not exists(folder):
         os.mkdir(folder)
@@ -27,11 +32,33 @@ def log(*argv):
     if not exists(master_log):
         with open(master_log, 'w'): pass
         
-    if not 
+    if not exists(today_log):
+        with open(today_log, 'w'): pass
     
-    #log = open('logs/log.txt', 'a')
+    flog = format_log(argv, sprint_log)
+
+    write_to_file(master_log, flog)
+    write_to_file(today_log, flog)
+
+def write_to_file(file, str):
+    file = open(file, 'a')
+    file.write(str)
+    file.close()
     
-    #log.write(+str+'\n')
-    #log.close()
+def format_log(argv, sprint_log):
+    rlog = ""
+    flog = ""
     
-log()
+    if sprint_log:
+        for i in argv:
+            rlog += str(i)
+        flog = str(datetime.now().replace(microsecond=0)) + ": " + rlog + '\n'
+        return flog
+    else:
+        for i in argv:
+            rlog += str(i)
+        flog = "(print) " + str(datetime.now().replace(microsecond=0)) + ": " + rlog + '\n'
+        return flog
+    
+log("hola amigos de youtube")
+sprint("bon dia amics meus")
