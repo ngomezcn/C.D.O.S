55ee6850d002cb764e162cc0cfa6fe21b9e48ac7
ngomezcn <naimgomezcn@gmail.com>
2022-03-04 18:45:01 +0100

    remove dumb file

--- src/dbm/tests/test_core.py
+++ src/dbm/tests/test_core.py
@@ -2,7 +2,6 @@ import pytest
 import unittest
 from sqlalchemy.ext.declarative import declarative_base
 from dbm.core.manager import *
-from dbm.core.headers import EXC, TRY
 
 class TestCore(unittest.TestCase):
 
@@ -12,10 +11,6 @@ class TestCore(unittest.TestCase):
         except Exception as exc:
             pytest.fail(pytrace=True, reason=exc.__cause__)
 
-  
-
 if __name__ == '__main__':
     unittest.main()
 
-
-from pprint import pprint
