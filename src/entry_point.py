76eec58fab298c3439d53d1ef7afc785f962a4d2
Naïm Gómez <naimgomezcn@gmail.com>
2022-04-30 15:43:15 +0200

    Update entry_point.py

--- src/entry_point.py
+++ src/entry_point.py
@@ -1,37 +1,15 @@
-#from dbm import CryptoTrackingPlatform, db
-
-#new_ctp = CryptoTrackingPlatform('coingecko', 'coingecko.com', '104.148.544.127', 'https://www.coingecko.com/')
-#db.cinsert(ne
-# w_ctp)
-#ctp = db.session.query(CryptoTrackingPlatform).get("coingecko")
-#print(ctp.domain)
-
-#from db.core.db_manager import db
-#from db.models.discovery_system import *
-
-#db.cinsert(CryptoTrackingPlatform('casd','asd','asdsad','asdsad'))
-
-#print(db.session.query(ScrapedToken).count())
-#from dbm.core.manager import db
-#from dbm.models.discovery_system import crypto_tracking_platform, scraped_token 
-#from dbm.models import tables
-
-
-#db.cinsert(tables.crypto_tracking_platform('casd','asd','asdsad','asdsad'))
-
-#print(db.session.query(ScrapedToken).count())
-
 from scraper.coingecko.coingecko_spider import CoingeckoSpider
 import scrapy
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
+
 configure_logging()
+
 settings = get_project_settings()
 runner = CrawlerRunner(settings)
 runner.crawl(CoingeckoSpider, input)
-##runner.crawl(AnotherSpider)
-d = runner.join()
-d.addBoth(lambda _: reactor.stop())
+
+runner.join().addBoth(lambda _: reactor.stop())
 reactor.run()
